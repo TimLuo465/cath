8d1e1bd5664b4cfe6628bde8edf9110d8394f175
Tim Luo <timluo465@gmail.com>
2018-05-15 23:11:28 +0800

    gcd

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cath
 Algorithm in javascript
+
+1. Euclidean algorithm - [gcd.js](./src/gcd.js)
+
+
---
+++ src/gcd.js
@@ -0,0 +1,9 @@
+/**
+ * Euclidean algorithm:
+ * An efficient method for computing the greatest common divisor (GCD) of two numbers
+ * ax+by = gcd(a, b) =d
+ */
+function gcd(p, q) {
+    if (q === 0) return p;
+    return gcd(q, p % q);
+}
