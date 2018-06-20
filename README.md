2013953b03f65b01ea2cc60a64387ad39e255a6b
timluo465 <1664311406@qq.com>
2018-06-20 09:23:54 +0800

    lzw

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ A repository about algorithm in javascript.
 1. Euclidean algorithm - [gcd.js](./src/math/gcd.js)
 
 ## Compress
-1. LZW
+
+1. LZW - [lzw.js](./src/compress/lzw.js)
 
 
--- src/compress/lzw.js
+++ src/compress/lzw.js
@@ -9,34 +9,34 @@ var LZW = {
             w = "",
             result = [],
             dictSize = 256;
-        for (i = 0; i < 256; i += 1) {
-            dictionary[String.fromCharCode(i)] = i;
-        }
- 
+
         for (i = 0; i < uncompressed.length; i += 1) {
             c = uncompressed.charAt(i);
             wc = w + c;
             //Do not use dictionary[wc] because javascript arrays 
             //will return values for array['pop'], array['push'] etc
-           // if (dictionary[wc]) {
             if (dictionary.hasOwnProperty(wc)) {
                 w = wc;
             } else {
-                result.push(dictionary[w]);
+                if (w && !dictionary[w]) {
+                    dictionary[w] = w.charCodeAt();
+                }
+
+                if (dictionary[w]) {
+                    result.push(dictionary[w]);
+                    dictionary[wc] = dictSize++;
+                }
                 // Add wc to the dictionary.
-                dictionary[wc] = dictSize++;
                 w = String(c);
             }
         }
- 
+
         // Output the code for w.
         if (w !== "") {
-            result.push(dictionary[w]);
+            result.push(dictionary[w] || w.charCodeAt());
         }
         return result;
     },
- 
- 
     decompress: function (compressed) {
         "use strict";
         // Build the dictionary.
@@ -47,10 +47,7 @@ var LZW = {
             k,
             entry = "",
             dictSize = 256;
-        for (i = 0; i < 256; i += 1) {
-            dictionary[i] = String.fromCharCode(i);
-        }
- 
+
         w = String.fromCharCode(compressed[0]);
         result = w;
         for (i = 1; i < compressed.length; i += 1) {
@@ -61,15 +58,15 @@ var LZW = {
                 if (k === dictSize) {
                     entry = w + w.charAt(0);
                 } else {
-                    return null;
+                    entry = String.fromCharCode(k);
                 }
             }
- 
+
             result += entry;
- 
+
             // Add w+entry[0] to the dictionary.
             dictionary[dictSize++] = w + entry.charAt(0);
- 
+
             w = entry;
         }
         return result;
